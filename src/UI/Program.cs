443fc1dffeea4376ef067126812d4c9d0031768f
Dilan Livera <dilan_yahampath@yahoo.com>
2025-10-16 22:03:57 +1100

    feat(image collection): connect to azure blob storage

--- src/UI/Program.cs
+++ src/UI/Program.cs
@@ -1,12 +1,17 @@
+using Aspire.ServiceDefaults;
 using Azure;
 using Azure.AI.Inference;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Qdrant.Client;
+using Qdrant.Client.Grpc;
 using UI;
 using UI.Components;
 using UI.Components.Pages;
+using UI.Data;
 using UI.Infrastructure;
 using UI.Infrastructure.Collections;
 using UI.Infrastructure.Models;
-using Aspire.ServiceDefaults;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
@@ -52,6 +57,15 @@ builder.Services.AddScoped<EmbeddingsClient>(sp =>
 
 builder.Services.AddSingleton<CollectionInitializationStatusManager>();
 
+builder.Services.AddSingleton<BlobContainerClient>(sp =>
+{
+    IConfiguration configuration = sp.GetRequiredService<IConfiguration>();
+    string connectionString = configuration.GetValue<string>(key: "AzureBlobStorage:ConnectionString") ?? throw new InvalidOperationException("'AzureBlobStorage:ConnectionString' configuration is not set");
+    string blobContainerName = configuration.GetValue<string>(key: "AzureBlobStorage:BlobContainerName") ?? throw new InvalidOperationException("'AzureBlobStorage:BlobContainerName' configuration is not set");
+
+    return new BlobContainerClient(connectionString, blobContainerName);
+});
+
 builder.AddServiceDefaults();
 
 WebApplication app = builder.Build();
@@ -65,13 +79,14 @@ _ = Task.Run(async () =>
         ILogger<Program> logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
         using (logger.BeginScope(state: "Starting collection initialization..."))
         {
-            ColorCollection colorCollection = scope.ServiceProvider.GetRequiredService<ColorCollection>();
-            VoidResult colorCollectionInitializationResult = await colorCollection.InitializeAsync();
 
-            if (colorCollectionInitializationResult.IsSuccess)
-            {
-                logger.LogDebug("'{CollectionName}' collection initialization completed successfully", nameof(ColorCollection));
-            }
+            ColorCollection colorCollection = scope.ServiceProvider.GetRequiredService<ColorCollection>();
+            // VoidResult colorCollectionInitializationResult = await colorCollection.InitializeAsync();
+            //
+            // if (colorCollectionInitializationResult.IsSuccess)
+            // {
+            //     logger.LogDebug("'{CollectionName}' collection initialization completed successfully", nameof(ColorCollection));
+            // }
 
             ImageCollection imageCollection = scope.ServiceProvider.GetRequiredService<ImageCollection>();
             VoidResult imageCollectionInitializationResult = await imageCollection.InitializeAsync();
@@ -79,6 +94,59 @@ _ = Task.Run(async () =>
             if (imageCollectionInitializationResult.IsSuccess)
             {
                 logger.LogDebug("'{CollectionName}' collection initialization completed successfully", nameof(ImageCollection));
+            }
+
+            BlobContainerClient blobContainerClient = scope.ServiceProvider.GetRequiredService<BlobContainerClient>();
+            Response<bool> doesContainerExist = await blobContainerClient.ExistsAsync();
+
+            if (!doesContainerExist)
+            {
+                logger.LogInformation("Blob container does not exist");
+            }
+
+            // logger.LogInformation("Listing blobs in container:");
+            // await foreach (BlobItem blob in  blobContainerClient.GetBlobsAsync())
+            // {
+            //     logger.LogInformation("- {BlobItemName} {BlobType}", blob.Name, "blob.Properties?.BlobType.Value");
+            // }
+
+            string blobName = AzureBlobStorageData.BlobNames[^305];
+            BlobClient blobClient = blobContainerClient.GetBlobClient(blobName);
+            Response<BlobDownloadResult> blobDownloadResult = await blobClient.DownloadContentAsync();
+
+            if (blobDownloadResult.HasValue)
+            {
+                BinaryData blobContent = blobDownloadResult.Value.Content;
+                string imageInBase64String = Convert.ToBase64String(blobContent.ToArray());
+
+                ImageEmbeddingInput input = new($"data:image/jpg;base64,{imageInBase64String}");
+
+                ImageEmbeddingsClient imageEmbeddingsClient = scope.ServiceProvider.GetRequiredService<ImageEmbeddingsClient>();
+                ImageEmbeddingsOptions options = new(input: [input])
+                                                 {
+
+                                                     InputType = EmbeddingInputType.Document, Model = "Cohere-embed-v3-english"
+                                                 };
+
+                Response<EmbeddingsResult> embeddingResult = await imageEmbeddingsClient.EmbedAsync(options);
+                float[] embedding = embeddingResult.Value
+                                                   .Data
+                                                   .Select(i => i.Embedding.ToObjectFromJson<float[]>() ?? throw new InvalidOperationException("Failed to deserialize embedding item."))
+                                                   .ToArray()
+                                                   .First();
+
+                PointStruct point = new()
+                                    {
+                                        Id = Guid.NewGuid(),
+                                        Vectors = embedding,
+                                        Payload =
+                                        {
+                                            ["image_in_base64_string"] = imageInBase64String, ["image_name"] = blobName, ["format"] = blobContent.MediaType ?? "", ["created_at_utc"] = DateTimeOffset.UtcNow.ToString()
+                                        }
+                                    };
+
+                QdrantClient qdrantClient = scope.ServiceProvider.GetRequiredService<QdrantClient>();
+                UpdateResult updateResult = await qdrantClient.UpsertAsync(collectionName: "images", points: [point]);
             }
         }
     }

c804297e1b278d522c28299fa7933c3c74f3b22e
dilan waduge <dilan.waduge@motorolasolutions.com>
2025-10-17 17:27:51 +1100

    feat(aspire): add config properties for bind mounts

--- src/Aspire.Host/AppHost.cs
+++ src/Aspire.Host/AppHost.cs
@@ -1,19 +1,21 @@
+using Microsoft.Extensions.Configuration;
+
 IDistributedApplicationBuilder builder = DistributedApplication.CreateBuilder(args);
 
 // https://learn.microsoft.com/en-us/dotnet/aspire/database/qdrant-integration?tabs=dotnet-cli
 IResourceBuilder<ParameterResource> qdrantApiKeyParameter = builder.AddParameter(name: "QdrantApiKey", secret: true);
+string qdrantDbBindMountSource = builder.Configuration.GetValue<string>(key: nameof(qdrantDbBindMountSource)) ?? throw new InvalidOperationException($"'{nameof(qdrantDbBindMountSource)}' configuration is not set");
+
 IResourceBuilder<QdrantServerResource> vectorDb = builder.AddQdrant(name: "qdrant-db", qdrantApiKeyParameter)
                                                          .WithContainerName("vector-search-demo-qdrant-db")
-                                                         .WithBindMount(source: "/Users/dilan_livera/dev/repositories/vector_search_demo/container_volumes/qdrant",
-                                                                        target: "/qdrant/storage")
+                                                         .WithBindMount(qdrantDbBindMountSource, target: "/qdrant/storage")
                                                          .WithLifetime(ContainerLifetime.Persistent);
 
 // https://learn.microsoft.com/en-us/dotnet/aspire/community-toolkit/ollama?tabs=dotnet-cli%2Cdocker
+string ollamaBindMountSource = builder.Configuration.GetValue<string>(key: nameof(ollamaBindMountSource)) ?? throw new InvalidOperationException($"'{nameof(ollamaBindMountSource)}' configuration is not set");
 IResourceBuilder<OllamaResource> ollama = builder.AddOllama(name: "ollama")
                                                  .WithContainerName("vector-search-demo-ollama")
-                                                 .WithExternalHttpEndpoints()
-                                                 .WithBindMount(source: "/Users/dilan_livera/dev/repositories/vector_search_demo/container_volumes/ollama",
-                                                                target: "/root/.ollama")
+                                                 .WithBindMount(ollamaBindMountSource, target: "/root/.ollama")
                                                  .WithLifetime(ContainerLifetime.Persistent);
 IResourceBuilder<OllamaModelResource> embedding = ollama.AddModel(name: "embedding",
                                                                   modelName: "mxbai-embed-large:335m");

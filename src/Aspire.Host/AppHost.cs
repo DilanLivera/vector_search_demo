5d09cfb5ba1c2209d75ebd0741d88b226e7c4ab1
Dilan Livera <dilan_yahampath@yahoo.com>
2025-10-17 18:34:16 +1100

    refac(aspire): rename container and model resource name

--- src/Aspire.Host/AppHost.cs
+++ src/Aspire.Host/AppHost.cs
@@ -6,24 +6,24 @@ IDistributedApplicationBuilder builder = DistributedApplication.CreateBuilder(ar
 IResourceBuilder<ParameterResource> qdrantApiKeyParameter = builder.AddParameter(name: "QdrantApiKey", secret: true);
 string qdrantDbBindMountSource = builder.Configuration.GetValue<string>(key: nameof(qdrantDbBindMountSource)) ?? throw new InvalidOperationException($"'{nameof(qdrantDbBindMountSource)}' configuration is not set");
 
-IResourceBuilder<QdrantServerResource> vectorDb = builder.AddQdrant(name: "qdrant-db", qdrantApiKeyParameter)
-                                                         .WithContainerName("vector-search-demo-qdrant-db")
+IResourceBuilder<QdrantServerResource> qdrantDb = builder.AddQdrant(name: "qdrant-db", qdrantApiKeyParameter)
+                                                         .WithContainerName("qdrant-db")
                                                          .WithBindMount(qdrantDbBindMountSource, target: "/qdrant/storage")
                                                          .WithLifetime(ContainerLifetime.Persistent);
 
 // https://learn.microsoft.com/en-us/dotnet/aspire/community-toolkit/ollama?tabs=dotnet-cli%2Cdocker
 string ollamaBindMountSource = builder.Configuration.GetValue<string>(key: nameof(ollamaBindMountSource)) ?? throw new InvalidOperationException($"'{nameof(ollamaBindMountSource)}' configuration is not set");
 IResourceBuilder<OllamaResource> ollama = builder.AddOllama(name: "ollama")
-                                                 .WithContainerName("vector-search-demo-ollama")
+                                                 .WithContainerName("ollama")
                                                  .WithBindMount(ollamaBindMountSource, target: "/root/.ollama")
                                                  .WithLifetime(ContainerLifetime.Persistent);
-IResourceBuilder<OllamaModelResource> embedding = ollama.AddModel(name: "embedding",
+IResourceBuilder<OllamaModelResource> embedding = ollama.AddModel(name: "embedding-model",
                                                                   modelName: "mxbai-embed-large:335m");
 
 IResourceBuilder<ProjectResource> ui = builder.AddProject<Projects.UI>(name: "ui")
-                                              .WithReference(vectorDb)
+                                              .WithReference(qdrantDb)
                                               .WithReference(embedding)
-                                              .WaitFor(vectorDb)
+                                              .WaitFor(qdrantDb)
                                               .WaitFor(embedding);
 
 builder.Build().Run();

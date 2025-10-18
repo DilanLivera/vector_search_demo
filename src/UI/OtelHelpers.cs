90380d20c84ea6a0abb2bdbe25765355f0976b42
Dilan Livera <dilan_yahampath@yahoo.com>
2025-10-18 17:15:07 +1100

    feat(images page): add activity for search

--- src/UI/OtelHelpers.cs
+++ src/UI/OtelHelpers.cs
@@ -16,6 +16,7 @@ public static class OtelHelpers
         public const string ColorCollectionInitializationMessage = $"{nameof(ColorCollection)}_Initialization";
         public const string ColorSearchVectorsMessage = "Color_Search.";
 
-        public const string ImageCollectionInitializationMessage =  $"{nameof(ImageCollection)}_Initialization";
+        public const string ImageCollectionInitializationMessage = $"{nameof(ImageCollection)}_Initialization";
+        public const string ImageSearchVectorsMessage = "Image_Search.";
     }
 }
